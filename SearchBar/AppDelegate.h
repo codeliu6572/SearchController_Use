a45ed93389175fd1fc3b6feec68dfeb5eda0fe05
codeliu6572 <codeliu6572@163.com>
2016-08-10 10:16:59 +0800

    Initial Commit

---
+++ SearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBar
+//
+//  Created by 刘浩浩 on 16/8/10.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchBar
+//
+//  Created by 刘浩浩 on 16/8/10.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
